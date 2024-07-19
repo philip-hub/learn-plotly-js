85dbb86a72085a18745e734ca87161f99abf6d10
philip-hub <philipstudentemail@gmail.com>
2024-07-19 13:01:39 -0400

    repaired with DOM loading

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let line1 = {
-    x:[1,2,3,4],
-    y:[10,15,13,17],
-    mode: 'markers',
-    type: 'scatter'
-};
+document.addEventListener('DOMContentLoaded', (event) => {
+    let line1 = {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        mode: 'markers',
+        type: 'scatter'
+    };
 
-let data = [line1]
+    let data = [line1];
 
-Plotly.newplot('scatterPlot',data)
+    Plotly.newPlot('scatterPlot', data);
+});
