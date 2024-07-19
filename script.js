f16000551ac273ba829d448e30b57c9442576f77
philip-hub <philipstudentemail@gmail.com>
2024-07-19 16:46:18 -0400

    refactored the code

--- script.js
+++ script.js
@@ -167,59 +167,73 @@ let layout2 = {
 
     Plotly.newPlot('funcPlot', [trace], layout4);
 
-    const chromoData = generateData(1000,24);
+    //const chromoData = generateData(1000,24);
 
-    const trace1 = {
-        x: chromoData.x,
-        y: chromoData.logRatio,
-        mode: 'markers',
-        type: 'scatter',
-        marker:{
-            size:2,
-            color: chromoData.chromosomes,
-            colorscale: 'Viridis'
-        },
-        name: 'Log Ratio'
-    };
+    // const trace1 = {
+    //     x: chromoData.x,
+    //     y: chromoData.logRatio,
+    //     mode: 'markers',
+    //     type: 'scatter',
+    //     marker:{
+    //         size:2,
+    //         color: chromoData.chromosomes,
+    //         colorscale: 'Viridis'
+    //     },
+    //     name: 'Log Ratio'
+    // };
 
-    const layout5 = {
-        title:'Log Ratio',
-        xaxis:{
-            title:'Genomic Position'
-        },
-        yaxis: {
-            title: 'log ratio',
-            range: [-2,2],
-        }
-    };
+    // const layout5 = {
+    //     title:'Log Ratio',
+    //     xaxis:{
+    //         title:'Genomic Position'
+    //     },
+    //     yaxis: {
+    //         title: 'log ratio',
+    //         range: [-2,2],
+    //     }
+    // };
 
-    Plotly.newPlot('plotLogRatio',[trace1],[layout5])
+    // Plotly.newPlot('plotLogRatio',[trace1],[layout5])
 
-    const trace2 = {
-        x:chromoData.x,
-        y:chromoData.bAlleleFrequency,
-        mode:'markers',
-        type:'scatter',
-        marker:{
-            size: 2,
-            color: chromoData.chromosomes,
-            colorscale: 'Viridis',
+    // const trace2 = {
+    //     x:chromoData.x,
+    //     y:chromoData.bAlleleFrequency,
+    //     mode:'markers',
+    //     type:'scatter',
+    //     marker:{
+    //         size: 2,
+    //         color: chromoData.chromosomes,
+    //         colorscale: 'Viridis',
 
-        },
-        name: 'B Allele Frequency'
-    }
+    //     },
+    //     name: 'B Allele Frequency'
+    // }
 
-    layout6 = {
-        title:"B Allele Frequency",
-        xaxis:{
-            title:"Genomic Position"
-        },
-        yaxis:{
-            title: "N Allele Frequency",
-            range: [0,1]
-        }
-    }
-    Plotly.newPlot("plotAllFreq",[trace2],[layout6])
+    // const layout6 = {
+    //     title: 'B Allele Frequency',
+    //     xaxis: {
+    //         title: 'Genomic Position'
+    //     },
+    //     yaxis: {
+    //         title: 'B Allele Frequency',
+    //         range: [0, 1]
+    //     },
+    //     legend: {
+    //         x: 1.05, // Position legend to the right of the plot
+    //         y: 0.5,
+    //         traceorder: 'normal',
+    //         font: {
+    //             family: 'sans-serif',
+    //             size: 12,
+    //             color: '#000'
+    //         },
+    //         bgcolor: '#E2E2E2',
+    //         bordercolor: '#FFFFFF',
+    //         borderwidth: 2
+    //     }
+    // };
+
+    // Plotly.newPlot("plotAllFreq",[trace2],[layout6])
 
 });
 
