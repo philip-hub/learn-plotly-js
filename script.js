01749cb548bfe1ba06d0fbbf1a111fe1c83d1743
philip-hub <philipstudentemail@gmail.com>
2024-07-19 14:40:19 -0400

    added interaction in the graph

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     let line1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
+        x: [1, 2, 3, 4, 5],
+        y: [1, 6, 3, 6, 1],
         mode: 'markers',
-        type: 'scatter'
+        type: 'scatter',
+        name: 'Team A',
+        text: ['A-1','A-2','A-3','A-4','A-5'],
+        textposistion: 'top center',
+        textfont:{
+            family: 'Raleway, Sans-serif'
+        },
+        marker: {size : 12}
+
     };
 
     let data = [line1];
@@ -14,8 +22,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let line2 = {
         x: [2,3,4,5],
         y: [16,5,11,9],
-        mode: 'lines',
+        mode: 'markers+text',
         type: 'scatter',
+        name: 'Team B',
+        text: ['B-1','B-2','B-3','B-4','B-5'],
+        textposistion: 'bottom center',
+        textfont:{
+            family: 'Raleway, Sans-serif'
+        },
+        marker: {size : 12}
     }
 
     let line3 = {
@@ -25,9 +40,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
         type:'scatter'
     }
 
-    let data2 = [line1, line2, line3]
+    let data2 = [line1, line2]
 
-    Plotly.newPlot('scatterPlot2', data2)
+
+    let layout = {
+        xaxis: {
+            range: [.75,5.25]
+        },
+        yaxis: {
+            range: [0,8]
+        },
+
+        legend:{
+            y: 0.5,
+            yref: 'paper',
+            font:{
+                family: 'Arial, sans-serif',
+                size: 20,
+                color: 'grey',
+            }
+        },
+
+        title:'Data Labels on the Plot'
+
+    };
+
+    Plotly.newPlot('scatterPlot2', data2,layout)
 
 
 });
